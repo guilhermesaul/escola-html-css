c72b00d05cf8756b63ea3d18ac9bd724a2fb268d
guisaul <guilhermesaul190@gmail.com>
2025-07-03 14:17:07 -0300

    Atividade 01 concluida

---
+++ design web/exercicios/pratica 03/ex001.js
@@ -0,0 +1,78 @@
+// Questão 1)
+// let, if e else
+
+// Questão 2)
+const saudacao = "Olá, mundo"
+var saudacoes = "Olá, mundo"
+
+// Questão 3)
+let s = "Romerito"
+let n = 1
+let u = undefined
+let nulo = null
+
+// Questão 4)
+let a = 4
+let b = 5
+let c = 6
+let media = (a+b+c)/3
+//
+// Questão 5)
+let Idade = 20
+if (Idade >= 18) {
+    console.log("Maior de idade")
+} else {
+    console.log("Menor de idade")
+}
+//
+// Questão 6)
+let dia = 5
+switch (dia) {
+    case 1:
+        console.log("Domingo")
+        break
+    case 2:
+        console.log("Segunda")
+        break
+    case 3:
+        console.log("Terça")
+        break
+    case 4:
+        console.log("Quarta")
+        break
+    case 5:
+        console.log("Quinta")
+        break
+    case 6:
+        console.log("Sexta")
+        break
+    case 7:
+        console.log("Sabado")
+        break
+}
+//
+// Questão 7)
+for (i=1; i<6; i++) {
+    console.log(i)
+}
+//
+// Questão 8)
+let ii = 10
+while (ii>0) {
+    console.log(ii)
+    ii--
+}
+//
+// Questão 9)
+const nome = "João";
+let idade = 20;
+console.log(`Meu nome é ${nome} e eu tenho ${idade} anos`);
+//
+// Questão 10)
+// Comentário de linha única
+/*
+comentario
+de
+múltiplas
+linhas
+*/
