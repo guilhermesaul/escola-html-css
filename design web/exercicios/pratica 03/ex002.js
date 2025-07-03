032bf8024709f8485229ddede19c3e8d86fe6d58
guisaul <guilhermesaul190@gmail.com>
2025-07-03 14:27:11 -0300

    Atividade 2 incompleta

---
+++ design web/exercicios/pratica 03/ex002.js
@@ -0,0 +1,40 @@
+// Questão 1)
+let num1 = 5;
+let num2 = 10;
+let soma = num1 + num2;
+console.log(soma);
+
+// Questão 2)
+let numero = 4;
+if (numero % 2 == 0) {
+  console.log("Número par");
+} else {
+  console.log("Número ímpar");
+}
+
+// Questão 3)
+let n1 = 10;
+let n2 = 56;
+let n3 = 30;
+if (n1 > n2 && n1 > n3) {
+  console.log(n1);
+}
+if (n2 > n1 && n2 > n3) {
+  console.log(n2);
+}
+if (n3 > n1 && n3 > n2) {
+  console.log(n3);
+}
+
+// Questão 4)
+for (i=1; i<11; i++) {
+    console.log(i)
+}
+
+// Questão 5)
+
+let fatorial = 5
+while (fatorial !=0 ) {
+    fatorial--
+    
+}
